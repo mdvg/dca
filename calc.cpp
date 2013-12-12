30301ff090ea0ec72933792e2712a0fdb91a27d0
Manulocal <correolocal@alu.ua.es>
2013-12-12 18:46:04 +0100

    Calculadora v1.2

--- calc.cpp
+++ calc.cpp
@@ -31,7 +31,7 @@ int main ()
 
      cout << "                     ************************************* \n"
 
-         << "                     * Calculadora basica para 2 numeros * \n"
+         << "                     * Calculadora basica para 2 numeros cambio más cosas* \n"
 
          << "                     ************************************* \n"
 
